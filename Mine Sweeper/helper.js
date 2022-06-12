849acab864e17b86861e4f1d7565ec270e17281d
atiyegh <72501292+atiyegh@users.noreply.github.com>
2022-06-12 21:48:24 +0430

    Add files via upload

---
+++ Mine Sweeper/helper.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+export default {
+    randomIntegers(n,min,max){
+        let randomNumber=null;
+        const result=[];
+        randomNumber=Math.round(min+Math.random()*(max-min))
+        console.log(randomNumber)
+    }
+}
